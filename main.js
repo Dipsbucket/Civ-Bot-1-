4b3f0d47ea69cfcbdfcfc798863c2dee5853f840
edmsimp <63891357+edmsimp@users.noreply.github.com>
2021-01-18 22:17:23 -0300

    First commit
    Bot first code

---
+++ main.js
@@ -0,0 +1,393 @@
+
+var Leaders = [
+    {
+        "Name": "Alexandre",
+        "Civilizacao": "MACEDÔNIA",
+        "Pick": false,
+        "Emoji": "<:Alexandre:799362245128618004>"
+    },
+    {
+        "Name": "Amanitore",
+        "Civilizacao": "NÚBIA",
+        "Pick": false,
+        "Emoji": "<:Amanitore:799365350042370049>"
+    },
+    {
+        "Name": "Ambiorix",
+        "Civilizacao": "GÁLIA",
+        "Pick": false,
+        "Emoji": "<:Ambiorix:799365348695736390>"
+    },
+    {
+        "Name": "Basílio II",
+        "Civilizacao": "IMPÉRIO BIZANTINO",
+        "Pick": false,
+        "Emoji": "<:Basilio:799366421654011924>"
+    },
+    {
+        "Name": "Catarina de Médici",
+        "Civilizacao": "FRANÇA",
+        "Pick": false,
+        "Emoji": "<:Catarina:799366423234347008>"
+    },
+    {
+        "Name": "Chandragupta",
+        "Civilizacao": "ÍNDIA",
+        "Pick": false,
+        "Emoji": "<:Chandragupta:799366421082537994>"
+    },
+    {
+        "Name": "Cleópatra",
+        "Civilizacao": "EGITO",
+        "Pick": false,
+        "Emoji": "<:Cleopatra:799366420516962324>"
+    },
+    {
+        "Name": "Ciro",
+        "Civilizacao": "PÉRSIA",
+        "Pick": false,
+        "Emoji": "<:Ciro:799366419434438666>"
+    },
+    {
+        "Name": "Cristina",
+        "Civilizacao": "SUÉCIA",
+        "Pick": false,
+        "Emoji": "<:Cristina:799366419334692864>"
+    },
+    {
+        "Name": "Dido",
+        "Civilizacao": "FENÍCIA",
+        "Pick": false,
+        "Emoji": "<:Dido:799366410409082911>"
+    },
+    {
+        "Name": "Dom Pedro II",
+        "Civilizacao": "BRASIL",
+        "Pick": false,
+        "Emoji": "<:DomPedro:799370866986713088>"
+    },
+    {
+        "Name": "Edviges",
+        "Civilizacao": "POLÔNIA",
+        "Pick": false,
+        "Emoji": "<:Edviges:799370875257225236>"
+    },
+    {
+        "Name": "Felipe II",
+        "Civilizacao": "ESPANHA",
+        "Pick": false,
+        "Emoji": "<:Filipe:799370873366642688>"
+    },
+    {
+        "Name": "Frederico",
+        "Civilizacao": "GERMÂNIA",
+        "Pick": false,
+        "Emoji": "<:Frederico:799376909464502283>"
+    },
+    {
+        "Name": "Gandhi",
+        "Civilizacao": "ÍNDIA",
+        "Pick": false,
+        "Emoji": "<:Gandhi_28Civ629:799370880471400528>"
+    },
+    {
+        "Name": "Gengis Khan",
+        "Civilizacao": "MONGÓLIA",
+        "Pick": false,
+        "Emoji": "<:Gengis:799377156521197569>"
+    },
+    {
+        "Name": "Gilgamesh",
+        "Civilizacao": "SUMÉRIA",
+        "Pick": false,
+        "Emoji": "<:Guilgamesh:799370873596674108>"
+    },
+    {
+        "Name": "Gitarja",
+        "Civilizacao": "INDONÉSIA",
+        "Pick": false,
+        "Emoji": "<:Gitarja:799370882534866994>"
+    },
+    {
+        "Name": "Gorgo",
+        "Civilizacao": "GRÉCIA",
+        "Pick": false,
+        "Emoji": "<:Gorgo:799370884007723028>   "
+    },
+    {
+        "Name": "Guilhermina",
+        "Civilizacao": "HOLANDA",
+        "Pick": false,
+        "Emoji": "<:Guilhermina:799370873815171143>"
+    },
+    {
+        "Name": "Hamurabi",
+        "Civilizacao": "BABILÔNIA",
+        "Pick": false,
+        "Emoji": "<:Hammurabi:799370872468144138>"
+    },
+    {
+        "Name": "Haroldo Hardrada",
+        "Civilizacao": "NORUEGA",
+        "Pick": false,
+        "Emoji": "<:Haroldo:799370871847518218>"
+    },
+    {
+        "Name": "Hojo Tokimune",
+        "Civilizacao": "JAPÃO",
+        "Pick": false,
+        "Emoji": "<:Hojo:799370867666452543>"
+    },
+    {
+        "Name": "Jayavarman VII",
+        "Civilizacao": "IMPÉRIO KHMER",
+        "Pick": false,
+        "Emoji": "<:Jayavarman_VII_28Civ629:799370880961871882>"
+    },
+    {
+        "Name": "John Curtin",
+        "Civilizacao": "AUSTRÁLIA",
+        "Pick": false,
+        "Emoji": "<:JohnCurtin:799370879997575231>"
+    },
+    {
+        "Name": "Kupe",
+        "Civilizacao": "MAORI",
+        "Pick": false,
+        "Emoji": "<:Kupe_28Civ629:799370881717370880>"
+    },
+    {
+        "Name": "Leonor da Aquitânia",
+        "Civilizacao": "FRANÇA",
+        "Pick": false,
+        "Emoji": "<:Eleanor:799370874007322634>"
+    },
+    {
+        "Name": "Leonor da Aquitânia",
+        "Civilizacao": "INGLATERRA",
+        "Pick": false,
+        "Emoji": "<:Eleanor:799370874007322634>"
+    },
+    {
+        "Name": "Lautaro",
+        "Civilizacao": "MAPUCHE",
+        "Pick": false,
+        "Emoji": "<:Lautaro:799370869369471036>"
+    },
+    {
+        "Name": "Mansa Musa",
+        "Civilizacao": "MALI",
+        "Pick": false,
+        "Emoji": "<:MansaMusa:799370871264641034>"
+    },
+    {
+        "Name": "Matias Corvino",
+        "Civilizacao": "HUNGRIA",
+        "Pick": false,
+        "Emoji": "<:MatiasCorvino:799370870966452295>"
+    },
+    {
+        "Name": "Menelik II",
+        "Civilizacao": "ETIÓPIA",
+        "Pick": false,
+        "Emoji": "<:Menelik_II_28Civ629:799370879913033738>"
+    },
+    {
+        "Name": "Montezuma",
+        "Civilizacao": "IMPÉRIO ASTECA",
+        "Pick": false,
+        "Emoji": "<:Montezuma:799370883919511553>"
+    },
+    {
+        "Name": "Mvemba Nzinga",
+        "Civilizacao": "CONGO",
+        "Pick": false,
+        "Emoji": "<:Mvemba:799370882182807552>"
+    },
+    {
+        "Name": "Pachacuti",
+        "Civilizacao": "IMPÉRIO INCA",
+        "Pick": false,
+        "Emoji": "<:Pachacuti:799370879309840424>"
+    },
+    {
+        "Name": "Pedro",
+        "Civilizacao": "RÚSSIA",
+        "Pick": false,
+        "Emoji": "<:Pedro:799370869125677116>"
+    },
+    {
+        "Name": "Péricles",
+        "Civilizacao": "GRÉCIA",
+        "Pick": false,
+        "Emoji": "<:Pericles:799370883168731167>"
+    },
+    {
+        "Name": "Qin Shi Huang",
+        "Civilizacao": "CHINA",
+        "Pick": false,
+        "Emoji": "<:QinShiHuang:799370881729036290>"
+    },
+    {
+        "Name": "Robert the Bruce",
+        "Civilizacao": "ESCÓCIA",
+        "Pick": false,
+        "Emoji": "<:TheBruce:799370874729529374>"
+    },
+    {
+        "Name": "Saladino",
+        "Civilizacao": "IMPÉRIO ÁRABE",
+        "Pick": false,
+        "Emoji": "<:Saladin:799370881197015040>"
+    },
+    {
+        "Name": "Seondeok",
+        "Civilizacao": "CORÉIA",
+        "Pick": false,
+        "Emoji": "<:Seondeok_28Civ629:799370882044002326>"
+    },
+    {
+        "Name": "Shaka",
+        "Civilizacao": "ZULULÂNDIA",
+        "Pick": false,
+        "Emoji": "<:Shaka:799370883906797618>"
+    },    
+    {
+        "Name": "Simon Bolivar",
+        "Civilizacao": "GRAN-COLÔMBIA",
+        "Pick": false,
+        "Emoji": "<:Bolivar:799370882052915240>"
+    },
+    {
+        "Name": "Solimão",
+        "Civilizacao": "IMPÉRIO OTOMANO",
+        "Pick": false,
+        "Emoji": "<:Suleiman:799370877400907865>"
+    },
+    {
+        "Name": "Tamar",
+        "Civilizacao": "GEÓRGIA",
+        "Pick": false,
+        "Emoji": "<:Tamar_28Civ629:799370883209756724>"
+    },
+    {
+        "Name": "Teddy Rosevelt",
+        "Civilizacao": "ESTADOS UNIDOS",
+        "Pick": false,
+        "Emoji": "<:Teddy:799370878688690176>"
+    },
+    {
+        "Name": "Tomyris",
+        "Civilizacao": "IMÉRIO CITA",
+        "Pick": false,
+        "Emoji": "<:Tomyris_28Civ629:799370877573267488>"
+    },
+    {
+        "Name": "Trajano",
+        "Civilizacao": "IMPÉRIO ROMANO",
+        "Pick": false,
+        "Emoji": "<:Trajano:799362297784565871>"
+    },
+    {
+        "Name": "Vitória",
+        "Civilizacao": "INGLATERRA",
+        "Pick": false,
+        "Emoji": "<:Victoria:799370880496435220>"
+    },
+    {
+        "Name": "Wac-Chanil-Ajaw",
+        "Civilizacao": "IMPÉRIO MAIA",
+        "Pick": false,
+        "Emoji": "<:Lady:799370882140209162>"
+    },
+    {
+        "Name": "Wilfrid Laurier",
+        "Civilizacao": "CANADÁ",
+        "Pick": false,
+        "Emoji": "<:Wilfrid_Laurier_28Civ629:799370873178030120>"
+    }
+]
+
+var Maps = ["Mar Interior", "Pangeia", "Sete Mares", "Continentes"]
+
+var pickedLeaders = [
+    {
+        "Jogador": 1,
+        "Líderes": []
+    },
+    {
+        "Jogador": 2,
+        "Líderes": []
+    },
+    {
+        "Jogador": 3,
+        "Líderes": []
+    },
+    {
+        "Jogador": 4,
+        "Líderes": []
+    },
+    {
+        "Jogador": 5,
+        "Líderes": []
+    },
+    {
+        "Jogador": 6,
+        "Líderes": []
+    },{
+        "Jogador": 7,
+        "Líderes": []
+    },{
+        "Jogador": 8,
+        "Líderes": []
+    },
+
+]
+
+var aux = [];
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+const prefix = '!';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.once('ready', () => {
+    console.log('CivFacilities is online!');
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length);
+    const messageArray = args.split(".");
+    const command = messageArray[0].toLowerCase();
+
+    if(command === 'draft'){
+        const numberOfPlayers = messageArray[1];
+        var i = 2;
+        var j = 0;
+        var bannedLeaders = [];
+        while(messageArray[i] != undefined)
+        {   
+            bannedLeaders[j] = messageArray[i];
+            j++;
+            i++;
+        }
+        client.commands.get('draft').execute(message, args, Leaders, Maps, pickedLeaders, aux,
+            numberOfPlayers, bannedLeaders, Discord)
+    }
+});
+
+client.login(process.env.token);
